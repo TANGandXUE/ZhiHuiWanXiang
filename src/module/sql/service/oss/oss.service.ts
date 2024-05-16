7cc5ae0c043ff6b2fdef9b3cec4ae6a91b8df1b3
TANGandXUE <tangxue@clouddreamai.com>
2024-05-16 16:41:48 +0800

    更新 oss.service.ts

--- src/module/sql/service/oss/oss.service.ts
+++ src/module/sql/service/oss/oss.service.ts
@@ -9,8 +9,8 @@ export class OssService {
     // 初始化OSS客户端。请将以下参数替换为您自己的配置信息。
     client = new OSS({
         region: 'oss-cn-shanghai', // 示例：'oss-cn-hangzhou'，填写Bucket所在地域。
-        accessKeyId: '',
-        accessKeySecret: '',
+        accessKeyId: ,
+        accessKeySecret: ,
         bucket: 'clouddreamai', // 示例：'my-bucket-name'，填写存储空间名称。
     });
 
