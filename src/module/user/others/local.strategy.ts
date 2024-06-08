18db4e732578a8df1bb5d9e953d52786a2620bb3
TANGandXUE <tangxue@clouddreamai.com>
2024-06-08 17:39:30 +0800

    修复bug

--- src/module/user/others/local.strategy.ts
+++ src/module/user/others/local.strategy.ts
@@ -9,17 +9,21 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
         super();
     }
 
-    async validate(userNameOrPhoneOrEmail: string, password: string): Promise<any> {
+    async validate(username: string, password: string): Promise<any> {
         // //打印输入的userNameOrPhoneOrEmail这个值是否为空，打印为true和false
         // if(userNameOrPhoneOrEmail == ''){
         //     console.log('userNameOrPhoneOrEmail不存在');
         // }
+        // 打印用户名和密码，前面加上名字和冒号
+        console.log('userNameOrPhoneOrEmail:',username);
+        console.log('userPassword:',password);
+
 
         
         
         const dto = await this.sqlService.validateUser({
-            userNameOrPhoneOrEmail,
-            userPassword:password,
+            userNameOrPhoneOrEmail: username,
+            userPassword: password,
         });
         if (typeof(dto) == 'string') {
             console.log(dto);
